a137190db4fc4ae4a6cce95547b0ad660e00af50
vishal <vishrrajput02@gmail.com>
2023-06-08 16:53:26 +0530

    java learnings initial commit

---
+++ src/FirstCode.java
@@ -0,0 +1,10 @@
+
+public class FirstCode {
+  public static void main(String args[])
+  {
+	  float a=5.5f;
+	  System.out.println(a);
+	  
+  }
+  
+}
---
+++ src/OperatorDemo.java
@@ -0,0 +1,13 @@
+
+public class OperatorDemo {
+
+	public static void main(String[] args) {
+    int i = 55;
+    int j=0;
+    
+    j=i>6?1:2;
+    System.out.println(j);
+    
+
+}
+}
